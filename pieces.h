8fdc3594f4c1d7cdff9e6fe2ba8fc24cc977b5db
Mayank Verma <vermamayank0412@gmail.com>
2026-05-25 00:54:45 +0530

    Update pieces.h

--- pieces.h
+++ pieces.h
@@ -1,1 +1,12 @@
-//pieces header
+#ifndef PIECES_H
+#define PIECES_H
+
+#define EMPTY '.'
+#define PAWN 'P'
+#define ROOK 'R'
+#define KNIGHT 'N'
+#define BISHOP 'B'
+#define QUEEN 'Q'
+#define KING 'K'
+
+#endif
