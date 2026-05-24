489f9beb63d2205ad93e095851787879d4ae4c37
Mayank Verma <vermamayank0412@gmail.com>
2026-05-25 00:58:08 +0530

    Update pieces.h

--- pieces.h
+++ pieces.h
@@ -2,11 +2,19 @@
 #define PIECES_H
 
 #define EMPTY '.'
-#define PAWN 'P'
-#define ROOK 'R'
-#define KNIGHT 'N'
-#define BISHOP 'B'
-#define QUEEN 'Q'
-#define KING 'K'
+
+#define WHITE_PAWN 'P'
+#define WHITE_ROOK 'R'
+#define WHITE_KNIGHT 'N'
+#define WHITE_BISHOP 'B'
+#define WHITE_QUEEN 'Q'
+#define WHITE_KING 'K'
+
+#define BLACK_PAWN 'p'
+#define BLACK_ROOK 'r'
+#define BLACK_KNIGHT 'n'
+#define BLACK_BISHOP 'b'
+#define BLACK_QUEEN 'q'
+#define BLACK_KING 'k'
 
 #endif
